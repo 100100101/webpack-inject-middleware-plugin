07fe6696579dd513e05d21d931c5445b37ece28e
JSerGit <bibik.serej@gmail.com>
2022-12-04 13:04:54 +0200

    develop dependencies

--- index.ts
+++ index.ts
@@ -1,22 +1,30 @@
-import * as dotenv from 'dotenv'
 import useNodeInspector from 'use-node-inspector'
 import { Compiler } from 'webpack'
-dotenv.config()
-
-const { NODE_INSPECTOR_PORT, IS_USE_NODE_INSPECTOR } = process.env
-if (IS_USE_NODE_INSPECTOR === 'true') {
-    let port = 0
-    if (NODE_INSPECTOR_PORT) {
-        port = parseInt(NODE_INSPECTOR_PORT) || 9229
-    }
-    useNodeInspector({
-        host: 'localhost',
-        port,
-    })
+export interface IOptions {
+    isUseNodeInspector?: boolean
+    nodeInspectorPort?: number
 }
 
 export class WebpackInjectMiddlewarePlugin {
+    private readonly isUseNodeInspector: IOptions['isUseNodeInspector']
+    private readonly nodeInspectorPort: IOptions['nodeInspectorPort']
+
+    constructor({
+        isUseNodeInspector = false,
+        nodeInspectorPort = 9229,
+    }: IOptions = {}) {
+        this.isUseNodeInspector = isUseNodeInspector
+        this.nodeInspectorPort = nodeInspectorPort
+    }
+
     apply(compiler: Compiler) {
+        if (this.isUseNodeInspector) {
+            const port = this.nodeInspectorPort
+            useNodeInspector({
+                host: 'localhost',
+                port,
+            })
+        }
         compiler.hooks.done.tap('webpack-inject-middleware-plugin', stats => {
             console.log('Hello webpack-inject-middleware-plugin')
         })
