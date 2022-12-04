8b292f8776a1a90e34cc0792eb2fe751a6591dd2
JSerGit <bibik.serej@gmail.com>
2022-12-04 20:32:45 +0200

    step

--- index.ts
+++ index.ts
@@ -1,10 +1,17 @@
 import { Compiler } from 'webpack'
+type TMiddleware = () => Promise<void>
+type TOptions = {
+    middleware: TMiddleware
+}
 export class WebpackInjectMiddlewarePlugin {
-    constructor() {
-        //
+    private readonly middleware: TMiddleware
+    constructor({ middleware }: TOptions) {
+        this.middleware = middleware
     }
 
     apply(compiler: Compiler) {
+        console.log('compiler:', compiler)
+
         compiler.hooks.done.tap('webpack-inject-middleware-plugin', stats => {
             console.log('Hello webpack-inject-middleware-plugin')
         })
