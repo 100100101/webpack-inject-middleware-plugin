66ee2a9afcc37baa67add503aa4f77e00d6371e3
JSerGit <bibik.serej@gmail.com>
2022-12-04 12:02:37 +0200

    add node inspector for developing

--- index.ts
+++ index.ts
@@ -2,15 +2,18 @@ import * as dotenv from 'dotenv'
 import useNodeInspector from 'use-node-inspector'
 import { Compiler } from 'webpack'
 dotenv.config()
-const port = process.env.NODE_INSPECTOR_PORT
-let portNumber = 0
-if (port) {
-    portNumber = parseInt(port) || 9229
+
+const { NODE_INSPECTOR_PORT, IS_USE_NODE_INSPECTOR } = process.env
+if (IS_USE_NODE_INSPECTOR === 'true') {
+    let port = 0
+    if (NODE_INSPECTOR_PORT) {
+        port = parseInt(NODE_INSPECTOR_PORT) || 9229
+    }
+    useNodeInspector({
+        host: 'localhost',
+        port,
+    })
 }
-useNodeInspector({
-    host: 'localhost',
-    port: portNumber,
-})
 
 export class WebpackInjectMiddlewarePlugin {
     apply(compiler: Compiler) {
