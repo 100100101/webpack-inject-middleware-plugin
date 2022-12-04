2035b8075716b82c50aacb720e9bc222188db122
JSerGit <bibik.serej@gmail.com>
2022-12-04 11:58:11 +0200

    add node inspector for developing

--- index.ts
+++ index.ts
@@ -1,4 +1,17 @@
+import * as dotenv from 'dotenv'
+import useNodeInspector from 'use-node-inspector'
 import { Compiler } from 'webpack'
+dotenv.config()
+const port = process.env.NODE_INSPECTOR_PORT
+let portNumber = 0
+if (port) {
+    portNumber = parseInt(port) || 9229
+}
+useNodeInspector({
+    host: 'localhost',
+    port: portNumber,
+})
+
 export class WebpackInjectMiddlewarePlugin {
     apply(compiler: Compiler) {
         compiler.hooks.done.tap('webpack-inject-middleware-plugin', stats => {
