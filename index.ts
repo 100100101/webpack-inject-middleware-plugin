7681884772ce73d6997d6996ec2e82799ff2745c
JSerGit <bibik.serej@gmail.com>
2022-12-04 13:44:21 +0200

    step

--- index.ts
+++ index.ts
@@ -1,30 +1,10 @@
-import useNodeInspector from 'use-node-inspector'
 import { Compiler } from 'webpack'
-export interface IOptions {
-    isUseNodeInspector?: boolean
-    nodeInspectorPort?: number
-}
-
 export class WebpackInjectMiddlewarePlugin {
-    private readonly isUseNodeInspector: IOptions['isUseNodeInspector']
-    private readonly nodeInspectorPort: IOptions['nodeInspectorPort']
-
-    constructor({
-        isUseNodeInspector = false,
-        nodeInspectorPort = 9229,
-    }: IOptions = {}) {
-        this.isUseNodeInspector = isUseNodeInspector
-        this.nodeInspectorPort = nodeInspectorPort
+    constructor() {
+        //
     }
 
     apply(compiler: Compiler) {
-        if (this.isUseNodeInspector) {
-            const port = this.nodeInspectorPort
-            useNodeInspector({
-                host: 'localhost',
-                port,
-            })
-        }
         compiler.hooks.done.tap('webpack-inject-middleware-plugin', stats => {
             console.log('Hello webpack-inject-middleware-plugin')
         })
